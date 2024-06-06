11b64b46487abb1c01a2e983965f7d140c076131
SKY-ADMIN <admin@sky.ngo>
2024-06-06 03:56:09 -0700

    added contact info

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <div className='footerNav' style={{
+            width: '75vw',
+            textAlign: 'center',
+            margin: '30px',
+            color: 'white'
+        }}>
+            <ul>
+                <li style={{ fontWeight: '600' }}>DM on viber for more Info</li>
+                <li style={{ fontWeight: '300', letterSpacing: '4px' }}>+960 7569695</li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default Footer
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Footer from "./Footer";
 
 const Home = () => {
   const [userField, setUserField] = useState({
@@ -53,7 +54,7 @@ const Home = () => {
             "https://api.sky.ngo/create",
             userField
           );
-          console.log(response)
+          console.log(response);
           Swal.fire({
             title: "Registration",
             text: "We have received the form, We will be in touch soon",
@@ -70,7 +71,7 @@ const Home = () => {
         }
       }
     });
-  }
+  };
   //   try {
   //     // const response = await axios.post(
   //     //   "https://api.sky.ngo/create",
@@ -96,273 +97,274 @@ const Home = () => {
   }
 
   return (
-    <div className="container">
-      <h2 className="header">
-        SKY PUBGM <br /> Eid League S1 Registration
-      </h2>
-      <form>
-        <div class="input-box">
-          <label>Team Name</label>
-          <input
-            type="text"
-            name="team_name"
-            placeholder="Enter team name"
-            required
-            onChange={(e) => changeUserFieldHandler(e)}
-          />
-        </div>
+    <>
+      <div className="container">
+        <h2 className="header">
+          SKY PUBGM <br /> Eid League S1 Registration
+        </h2>
+        <form>
+          <div class="input-box">
+            <label>Team Name</label>
+            <input
+              type="text"
+              name="team_name"
+              placeholder="Enter team name"
+              required
+              onChange={(e) => changeUserFieldHandler(e)}
+            />
+          </div>
 
-        <div class="input-box">
-          <label>IGL Name</label>
-          <input
-            type="text"
-            name="f_name"
-            placeholder="Enter IGL Name"
-            required
-            onChange={(e) => changeUserFieldHandler(e)}
-          />
-        </div>
+          <div class="input-box">
+            <label>IGL Name</label>
+            <input
+              type="text"
+              name="f_name"
+              placeholder="Enter IGL Name"
+              required
+              onChange={(e) => changeUserFieldHandler(e)}
+            />
+          </div>
 
-        <div class="input-box" style={{ textTransform: "capitalize" }}>
-          <label>NID No</label>
-          <input
-            type="text"
-            name="nid"
-            placeholder="Enter NID number"
-            required
-            onChange={(e) => changeUserFieldHandler(e)}
-            maxLength={6}
-          />
-        </div>
+          <div class="input-box" style={{ textTransform: "capitalize" }}>
+            <label>NID No</label>
+            <input
+              type="text"
+              name="nid"
+              placeholder="Enter NID number"
+              required
+              onChange={(e) => changeUserFieldHandler(e)}
+              maxLength={6}
+            />
+          </div>
 
-        <div class="input-box">
-          <label>Viber Number</label>
-          <input
-            type="text"
-            name="viber_number"
-            placeholder="Enter IGL viber number"
-            required
-            onChange={(e) => changeUserFieldHandler(e)}
-            maxLength={7}
-          />
-        </div>
+          <div class="input-box">
+            <label>Viber Number</label>
+            <input
+              type="text"
+              name="viber_number"
+              placeholder="Enter IGL viber number"
+              required
+              onChange={(e) => changeUserFieldHandler(e)}
+              maxLength={7}
+            />
+          </div>
 
-        <div class="input-box">
-          <label for="email">Email Address</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Enter email address"
-            required
-            onChange={(e) => changeUserFieldHandler(e)}
-          />
-        </div>
+          <div class="input-box">
+            <label for="email">Email Address</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter email address"
+              required
+              onChange={(e) => changeUserFieldHandler(e)}
+            />
+          </div>
 
-        <div class="player-reg-container">
+          <div class="player-reg-container">
+            <br />
+            <label class="player-label">Player 1</label>
+            <div class="column">
+              <div class="input-box">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  name="p1_fullname"
+                  placeholder="Enter full name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>NID Number</label>
+                <input
+                  type="text"
+                  name="p1_nid"
+                  placeholder="Enter NID"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={6}
+                />
+              </div>
+            </div>
+            <div class="column">
+              <div class="input-box">
+                <label>In-Game Name</label>
+                <input
+                  type="text"
+                  name="p1_ign"
+                  placeholder="Enter In-Game Name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>Game ID</label>
+                <input
+                  type="text"
+                  name="p1_igid"
+                  placeholder="PUBG ID Number"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={8}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="player-reg-container">
+            <br />
+            <label class="player-label">Player 2</label>
+            <div class="column">
+              <div class="input-box">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  name="p2_fullname"
+                  placeholder="Enter full name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>NID Number</label>
+                <input
+                  type="text"
+                  name="p2_nid"
+                  placeholder="Enter NID"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={6}
+                />
+              </div>
+            </div>
+            <div class="column">
+              <div class="input-box">
+                <label>In-Game Name</label>
+                <input
+                  type="text"
+                  name="p2_ign"
+                  placeholder="Enter In-Game Name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>Game ID</label>
+                <input
+                  type="text"
+                  name="p2_igid"
+                  placeholder="PUBG ID Number"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={8}
+                />
+              </div>
+            </div>
+          </div>
+          <div class="player-reg-container">
+            <br />
+            <label class="player-label">Player 3</label>
+            <div class="column">
+              <div class="input-box">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  name="p3_fullname"
+                  placeholder="Enter full name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>NID Number</label>
+                <input
+                  type="text"
+                  name="p3_nid"
+                  placeholder="Enter NID"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={6}
+                />
+              </div>
+            </div>
+            <div class="column">
+              <div class="input-box">
+                <label>In-Game Name</label>
+                <input
+                  type="text"
+                  name="p3_ign"
+                  placeholder="Enter In-Game Name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>Game ID</label>
+                <input
+                  type="text"
+                  name="p3_igid"
+                  placeholder="PUBG ID Number"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={8}
+                />
+              </div>
+            </div>
+          </div>
+          <div class="player-reg-container">
+            <br />
+            <label class="player-label">Player 4</label>
+            <div class="column">
+              <div class="input-box">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  name="p4_fullname"
+                  placeholder="Enter full name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>NID Number</label>
+                <input
+                  type="text"
+                  name="p4_nid"
+                  placeholder="Enter NID"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={6}
+                />
+              </div>
+            </div>
+            <div class="column">
+              <div class="input-box">
+                <label>In-Game Name</label>
+                <input
+                  type="text"
+                  name="p4_ign"
+                  placeholder="Enter In-Game Name"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                />
+              </div>
+              <div class="input-box">
+                <label>Game ID</label>
+                <input
+                  type="text"
+                  name="p4_igid"
+                  placeholder="PUBG ID Number"
+                  required
+                  onChange={(e) => changeUserFieldHandler(e)}
+                  maxLength={8}
+                />
+              </div>
+            </div>
+          </div>
+
           <br />
-          <label class="player-label">Player 1</label>
-          <div class="column">
-            <div class="input-box">
-              <label>Full Name</label>
-              <input
-                type="text"
-                name="p1_fullname"
-                placeholder="Enter full name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>NID Number</label>
-              <input
-                type="text"
-                name="p1_nid"
-                placeholder="Enter NID"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={6}
-              />
-            </div>
-          </div>
-          <div class="column">
-            <div class="input-box">
-              <label>In-Game Name</label>
-              <input
-                type="text"
-                name="p1_ign"
-                placeholder="Enter In-Game Name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>Game ID</label>
-              <input
-                type="text"
-                name="p1_igid"
-                placeholder="PUBG ID Number"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={8}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div class="player-reg-container">
-          <br />
-          <label class="player-label">Player 2</label>
-          <div class="column">
-            <div class="input-box">
-              <label>Full Name</label>
-              <input
-                type="text"
-                name="p2_fullname"
-                placeholder="Enter full name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>NID Number</label>
-              <input
-                type="text"
-                name="p2_nid"
-                placeholder="Enter NID"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={6}
-              />
-            </div>
-          </div>
-          <div class="column">
-            <div class="input-box">
-              <label>In-Game Name</label>
-              <input
-                type="text"
-                name="p2_ign"
-                placeholder="Enter In-Game Name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>Game ID</label>
-              <input
-                type="text"
-                name="p2_igid"
-                placeholder="PUBG ID Number"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={8}
-              />
-            </div>
-          </div>
-        </div>
-        <div class="player-reg-container">
-          <br />
-          <label class="player-label">Player 3</label>
-          <div class="column">
-            <div class="input-box">
-              <label>Full Name</label>
-              <input
-                type="text"
-                name="p3_fullname"
-                placeholder="Enter full name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>NID Number</label>
-              <input
-                type="text"
-                name="p3_nid"
-                placeholder="Enter NID"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={6}
-              />
-            </div>
-          </div>
-          <div class="column">
-            <div class="input-box">
-              <label>In-Game Name</label>
-              <input
-                type="text"
-                name="p3_ign"
-                placeholder="Enter In-Game Name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>Game ID</label>
-              <input
-                type="text"
-                name="p3_igid"
-                placeholder="PUBG ID Number"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={8}
-              />
-            </div>
-          </div>
-        </div>
-        <div class="player-reg-container">
-          <br />
-          <label class="player-label">Player 4</label>
-          <div class="column">
-            <div class="input-box">
-              <label>Full Name</label>
-              <input
-                type="text"
-                name="p4_fullname"
-                placeholder="Enter full name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>NID Number</label>
-              <input
-                type="text"
-                name="p4_nid"
-                placeholder="Enter NID"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={6}
-              />
-            </div>
-          </div>
-          <div class="column">
-            <div class="input-box">
-              <label>In-Game Name</label>
-              <input
-                type="text"
-                name="p4_ign"
-                placeholder="Enter In-Game Name"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-              />
-            </div>
-            <div class="input-box">
-              <label>Game ID</label>
-              <input
-                type="text"
-                name="p4_igid"
-                placeholder="PUBG ID Number"
-                required
-                onChange={(e) => changeUserFieldHandler(e)}
-                maxLength={8}
-              />
-            </div>
-          </div>
-        </div>
-
-        <br />
-        {/* <header>Substitutes (optional)</header>
+          {/* <header>Substitutes (optional)</header>
             <div class="player-reg-container">
               <br />
               <label class="player-label">Player 1</label>
@@ -443,18 +445,17 @@ const Home = () => {
               </div>
             </div> */}
 
-        <button
-          type="submit"
-          className="button"
-          onClick={(e) => onSubmitChange(e)}
-        >
-          Register Now
-        </button>
-      </form>
-      {/* <div className='col-md-8'>
-                        <List />
-                    </div> */}
-    </div>
+          <button
+            type="submit"
+            className="button"
+            onClick={(e) => onSubmitChange(e)}
+          >
+            Register Now
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 };
 
