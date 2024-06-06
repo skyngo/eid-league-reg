70a00b767b4a1b0b5a0d379acd2e810bc40e547b
SKY-ADMIN <admin@sky.ngo>
2024-06-06 04:44:29 -0700

    added copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from "react";
+
+const d = new Date();
+let year = d.getFullYear();
 
 const Footer = () => {
   return (
     <>
-        <div className='footerNav' style={{
-            width: '75vw',
-            textAlign: 'center',
-            margin: '30px',
-            color: 'white'
-        }}>
-            <ul>
-                <li style={{ fontWeight: '600' }}>DM on viber for more Info</li>
-                <li style={{ fontWeight: '300', letterSpacing: '4px' }}>+960 7569695</li>
-            </ul>
-        </div>
+      <div
+        className="footerNav"
+        style={{
+          width: "80vw",
+          textAlign: "center",
+          marginTop: "10vh",
+          color: "white",
+        }}
+      >
+        <ul>
+          <li style={{ fontWeight: "600" }}>DM on viber for more Info</li>
+          <li style={{ fontWeight: "300", letterSpacing: "4px" }}>
+            +960 7569695 | +960 7665552
+          </li>
+          <li style={{ fontWeight: "200", marginTop: '5vh', fontSize: '0.8rem' }}>
+            Copyright&#169;{year} Society for Kaashidhoo Youth
+          </li>
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
